697e59fb4100b15b36dc6c54b14528e3de9d1250
Eric Nohara-LeClair <ernohara@bu.edu>
2025-06-28 00:56:25 -0500

    Added optimizations to both functions

--- lambda_get_links/scrape_links.py
+++ lambda_get_links/scrape_links.py
@@ -6,7 +6,7 @@ urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 def find_recipe_links(dish_name, url=None, max_pages=3, page=1, collected=None):
     # base cases
     if collected is None:
-        collected = []
+        collected = set()
     if page > max_pages:
         return collected
 
@@ -15,25 +15,16 @@ def find_recipe_links(dish_name, url=None, max_pages=3, page=1, collected=None):
         url = f"https://www.allrecipes.com/search?q={query}"
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, verify=False)
-    soup = BeautifulSoup(response.text, "html.parser")
-
-    # Remove all script and style elements
-    for script_or_style in soup(['script', 'style']):
-        script_or_style.decompose()
+    soup = BeautifulSoup(response.text, "lxml")
 
     # get the important links
-    next_link = None
-    for a in soup.find_all("a"):
-        href = a.get("href", "")
-        text = a.get_text(strip=True)
-        if href and "/recipe/" in href and text:
-            collected.append(href)
+    for link in soup.select(".mntl-card-list-card--extendable"):
+        collected.add(link.get("href"))
 
-        span = a.find("span")
-        if span and span.get_text(strip=True).lower() == "next":
-            next_link = a
+    el = soup.select_one(".mntl-pagination__next")
 
-    # recurse
-    if next_link and next_link.get("href"):
-        return find_recipe_links(dish_name, next_link.get("href"), max_pages, page + 1, collected)
-    return collected
+    if (el):
+        href = el.find_next("a").get("href")
+        return find_recipe_links(dish_name, href, max_pages, page + 1, collected)
+        
+    return list(collected)
