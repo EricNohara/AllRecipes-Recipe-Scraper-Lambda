759e77ddf51332ff3e53b5597dcdfb4179a137cb
Eric Nohara-LeClair <ernohara@bu.edu>
2025-06-28 20:53:57 -0500

    fix

--- lambda_get_recipes/lambda_function.py
+++ lambda_get_recipes/lambda_function.py
@@ -5,16 +5,14 @@ from scrape_recipes import find_recipe_data
 from scrape_links import find_recipe_links
 
 async def fetch_recipe(session, link):
-    # call your async recipe data fetcher here
     return await find_recipe_data(session, link)
 
 async def main(dish, max_links):
     async with aiohttp.ClientSession() as session:
         links = find_recipe_links(dish_name=dish, max_links=max_links)
-        # tasks = [fetch_recipe(session, link) for link in links]
-        # recipes = await asyncio.gather(*tasks)
-        # return recipes
-        return links
+        tasks = [fetch_recipe(session, link) for link in links]
+        recipes = await asyncio.gather(*tasks)
+        return recipes
 
 def lambda_handler(event, context):
     try:
