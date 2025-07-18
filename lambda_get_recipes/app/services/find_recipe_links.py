2994a6ebb8b5cba51d0c2f5adb850a28df9483cc
Eric Nohara-LeClair <Eric.NoharaLeClair@dotfoods.com>
2025-07-18 08:40:11 -0500

    Fix imports in data links

--- lambda_get_recipes/app/services/find_recipe_links.py
+++ lambda_get_recipes/app/services/find_recipe_links.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
-from link_parsers import allrecipes, simplyrecipes, seriouseats
-from links_map import get_search_url
+from services.link_parsers import allrecipes, simplyrecipes, seriouseats
+from services.links_map import get_search_url
 import requests
 import urllib3
 
