ed7837a01a442985658c3025158b84536d3aa1fa
Eric Nohara-LeClair <Eric.NoharaLeClair@dotfoods.com>
2025-07-18 08:39:00 -0500

    Add link parser for seriouseats

--- lambda_get_recipes/app/services/find_recipe_links.py
+++ lambda_get_recipes/app/services/find_recipe_links.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
-from services.link_parsers import allrecipes, simplyrecipes
-from services.links_map import get_search_url
+from link_parsers import allrecipes, simplyrecipes, seriouseats
+from links_map import get_search_url
 import requests
 import urllib3
 
@@ -12,8 +12,12 @@ PARSERS = {
         "get_next_page_url": allrecipes.get_next_page_url
     },
     "simply-recipes": {
-       "parse_links": simplyrecipes.parse_links,
+        "parse_links": simplyrecipes.parse_links,
         "get_next_page_url": simplyrecipes.get_next_page_url 
+    },
+    "serious-eats": {
+        "parse_links": seriouseats.parse_links,
+        "get_next_page_url": seriouseats.get_next_page_url 
     }
 }
 
@@ -41,7 +45,7 @@ def find_recipe_links(dish_name, url=None, max_links=20, collected=None, sitenam
     next_url = parser["get_next_page_url"](soup)
     if next_url:
         return find_recipe_links(dish_name, next_url, max_links, collected, sitename)
-        
+            
     return list(collected)[:max_links]
 
-# print(find_recipe_links(dish_name="pizza", max_links=100, sitename="simply-recipes"))
+# print(find_recipe_links(dish_name="pizza", max_links=100, sitename="serious-eats"))
---
+++ lambda_get_recipes/app/services/link_parsers/seriouseats.py
@@ -0,0 +1,18 @@
+RECIPE_CARD = ".comp.card-list__item.mntl-block"
+RECIPE_LINK = "a.comp.card"
+NEXT_LINK_BUTTON = ".pagination__item-link--next"
+
+def parse_links(soup):
+    links = set()
+
+    for a in soup.select(RECIPE_LINK):
+        href = a.get('href', '')
+        if 'recipe' not in href or 'recipes' in href:
+            continue
+        links.add(href)
+    return list(links)
+
+def get_next_page_url(soup):
+    next_el = soup.select_one(NEXT_LINK_BUTTON)
+    href = next_el.get("href") if next_el else None
+    return href if href else None
